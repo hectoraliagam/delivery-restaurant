c41e23de916b920ed3c53f6d72c06fd55cdfb041
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-11-25 21:40:15 -0500

    navegación navbar menu

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("home");
+
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo"></img>
       <ul className="navbar-menu">
-        <li>Inicio</li>
-        <li>Menú</li>
-        <li>Aplicación</li>
-        <li>Contáctanos</li>
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Inicio</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menú</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Aplicación</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contáctanos</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
