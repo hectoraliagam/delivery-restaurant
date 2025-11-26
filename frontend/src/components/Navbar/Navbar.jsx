30a50e51c8ceabd89e0207f5b7ff23a270e371b9
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-11-26 17:11:10 -0500

    add Links in Navbar, prepare Cart section

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo"></img>
+      <Link to='/'><img src={assets.logo} alt="" className="logo"></img></Link>
       <ul className="navbar-menu">
         <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Inicio</Link>
         <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menú</a>
@@ -19,7 +19,7 @@ const Navbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className="dot"></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Regístrate</button>
