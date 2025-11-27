25198aef8aee86e333270c0c40c9e1a9744e03ef
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-11-26 22:17:21 -0500

    interactive food api

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,6 +1,6 @@
-import express from "express"
-import { addFood } from "../controllers/foodController.js"
-import multer from "multer"
+import express from "express";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
+import multer from "multer";
 
 
 const foodRouter = express.Router();
@@ -10,14 +10,15 @@ const foodRouter = express.Router();
 const storage = multer.diskStorage({
   destination: "uploads",
   filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`)
+    return cb(null, `${Date.now()}${file.originalname}`);
   }
-})
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
-foodRouter.post("/add", upload.single("image"), addFood)
-
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 
 export default foodRouter;
